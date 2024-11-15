3f62db41124c76583f3157dbc185d10da38a2cb7
jota-br <ojcroveda@gmail.com>
2024-11-15 14:08:22 -0300

    Lambda

--- LambdaExpressionsChallenge/src/dev/lpa/Main.java
+++ LambdaExpressionsChallenge/src/dev/lpa/Main.java
@@ -2,6 +2,9 @@ package dev.lpa;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class Main {
 
@@ -39,5 +42,59 @@ public class Main {
         };
 
         printWordsConcise.accept("Let's split this up into an array");
+
+        System.out.println("-".repeat(30));
+
+        UnaryOperator<String> everySecondChar = source -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
+        Function<String, String> everySecondChar2 = source -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < source.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(source.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
+        String result = everySecondChar.apply("1234567890");
+        String result2 = everySecondChar2.apply("1234567890");
+        System.out.println(everySecondChar.apply("1234567890"));
+        System.out.println(everySecondChar2.apply("1234567890"));
+        System.out.println(result);
+        System.out.println(result2);
+
+        String result3 = everySecondCharacter(everySecondChar, "1234567890");
+        System.out.println(result3);
+
+        Supplier<String> iLoveJava = () -> "I Love Java!";
+        Supplier<String> iLoveJava2 = () -> {
+            return "I Love Java!";
+        };
+        System.out.println(iLoveJava.get());
+    }
+
+    public static String everySecondChar(String source) {
+
+        StringBuilder returnVal = new StringBuilder();
+        for (int i = 0; i < source.length(); i++) {
+            if (i % 2 == 1) {
+                returnVal.append(source.charAt(i));
+            }
+        }
+        return returnVal.toString();
+    }
+
+    public static String everySecondCharacter(UnaryOperator<String> u, String source) {
+
+        return u.apply(source);
     }
 }
