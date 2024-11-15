9ea19d933c493e6dbcea4f18fe0cf4bd93c70c43
jota-br <ojcroveda@gmail.com>
2024-11-15 20:34:24 -0300

    Lambda & Method Refference

--- LambdaAndMethodChallenge/src/dev/lpa/Main.java
+++ LambdaAndMethodChallenge/src/dev/lpa/Main.java
@@ -19,6 +19,9 @@ public class Main {
         UnaryOperator<String> toLowerCaseExceptFirstLetter = source -> {
 
             String[] tmp = splitString(source);
+            if (tmp.length == 1) {
+                return source;
+            }
             return new StringBuilder(
                     tmp[0].charAt(0) + tmp[0].substring(1).transform(toLowerCase) + " " + tmp[1] + " "
                             + tmp[2].charAt(0) + tmp[2].substring(1).transform(toLowerCase)).toString();
@@ -27,6 +30,9 @@ public class Main {
         UnaryOperator<String> toPascalCase = source -> {
 
             String[] tmp = splitString(source);
+            if (tmp.length == 1) {
+                return source;
+            }
             return new StringBuilder(
                     tmp[0].charAt(0) + tmp[0].substring(1).transform(toLowerCase) + tmp[1].charAt(0)
                             + tmp[2].charAt(0) + tmp[2].substring(1).transform(toLowerCase)
@@ -39,15 +45,29 @@ public class Main {
             return parts[0].equalsIgnoreCase(parts[2]);
         };
 
-
         String[] arrayListOfNames = {"Anna", "Bob", "Eve", "Otto", "Adam", "Asa", "Lil", "Nany", "Pip", "Ellen"};
 //        Arrays.asList(arrayListOfNames).forEach(System.out::println);
         System.out.println("Original Array");
         System.out.println(Arrays.toString(arrayListOfNames));
 
+        List<UnaryOperator<String>> operations = new ArrayList<>(List.of(
+                String::toLowerCase,
+                String::toUpperCase,
+                s -> s + " " + generateRandomChar('A', 'Z') + ".",
+                s -> s + " " + reverseName(arrayListOfNames[new Random().nextInt(arrayListOfNames.length)]),
+                String::toLowerCase,
+                String::toUpperCase,
+                toLowerCaseExceptFirstLetter,
+                toPascalCase
+        ));
+
+        List<String> listOfNames = new ArrayList<>(List.of(arrayListOfNames));
+        applyChanges(listOfNames, operations);
+
         System.out.println("toUpperCase");
         Arrays.setAll(arrayListOfNames, i -> arrayListOfNames[i].transform(toUpperCase));
-        List<String> listOfNames = new ArrayList<>(List.of(arrayListOfNames));
+
+        listOfNames = new ArrayList<>(List.of(arrayListOfNames));
         listOfNames.forEach(System.out::println);
 
 //        listOfNames.replaceAll(s -> applyChanges(s, toUpperCase));
@@ -72,10 +92,16 @@ public class Main {
         System.out.println("Transform toPascalCase");
         listOfNames.replaceAll(s -> s.transform(toPascalCase));
         listOfNames.forEach(System.out::println);
+
+        System.out.println("-".repeat(30));
     }
 
-    public static <T> T applyChanges(T t, UnaryOperator<T> t2) {
-        return t2.apply(t);
+    public static void applyChanges(List<String> list, List<UnaryOperator<String>> functions) {
+
+        for (var function : functions) {
+            list.replaceAll(s -> s.transform(function));
+            System.out.println(list);
+        }
     }
 
     public static char generateRandomChar(char start, char end) {
